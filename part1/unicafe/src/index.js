0851f654833c88d05f2b7020d53408d0ef32f79e
Nadine-Fadlallah <nadine.fadlallah98@gmail.com>
2020-10-14 00:20:14 +0200

    Exercise 1.6 Solution

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = ({text}) => <h1>{text}</h1>
+const Button = ({handleClick, text}) => <button onClick={handleClick} >{text}</button>
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
@@ -8,9 +11,21 @@ const App = () => {
   const [bad, setBad] = useState(0)
 
   return (
-    <div>
-      code here
-    </div>
+    <>
+      
+    <Header text={"give feedback"}/>
+
+    <Button handleClick={()=>setGood(good+1)} text={"good"}/>
+    <Button handleClick={()=>setNeutral(neutral+1)} text={"neutral"}/>
+    <Button handleClick={()=>setBad(bad+1)} text={"bad"}/>
+
+    <Header text={"statistics"}/>
+
+    <div>good {good}</div>
+    <div>neutral {neutral}</div>
+    <div>bad {bad}</div>
+
+    </>
   )
 }
 
