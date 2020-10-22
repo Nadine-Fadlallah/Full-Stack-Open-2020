919adb9b897e0af69c602d45a630586d1360b1d2
Nadine-Fadlallah <nadine.fadlallah98@gmail.com>
2020-10-22 23:08:05 +0200

    Exercise 2.10 Solution

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,43 +1,47 @@
 import React, { useState } from "react"
 import Contacts from "./components/Contacts"
+import Form from "./components/Form"
+import Filter from "./components/Filter"
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" }
   ])
+
   const [newName, setNewName] = useState("")
   const [newNumber, setNewNumber] = useState("")
-  const[search, setNewSearch] = useState("")
-  
+  const [search, setNewSearch] = useState("")
 
   const addContact = (event) => {
     event.preventDefault()
 
     const repeatedName = persons
       .filter((person) => person.name === newName)
-      .map((hit) => hit.name).join()
+      .map((hit) => hit.name)
+      .join()
 
     const repeatedNumber = persons
       .filter((person) => person.number === newNumber)
-      .map((hit) => hit.number).join()
+      .map((hit) => hit.number)
+      .join()
     // Check no repetion or empty field
     if (!(repeatedName || repeatedNumber)) {
-  // show a message if one of the inputs is empty or both and don't reset
+      // show a message if one of the inputs is empty or both and don't reset
       if (!(newName && newNumber)) {
         window.alert("Please fill the missing contact info!")
-        return;}
+        return
+      }
 
       const personObj = {
         name: newName,
         number: newNumber
       }
-
       setPersons(persons.concat(personObj))
-    } 
-      
+    }
+    // In case of a repeated name or number
     else {
       window.alert(
         `${
@@ -48,7 +52,6 @@ const App = () => {
     //console.log("Repeated: " + repeatedName + repeatedNumber)
     setNewName("")
     setNewNumber("")
-   
   }
   const handleNameChange = (event) => {
     console.log(event.target.value)
@@ -65,36 +68,39 @@ const App = () => {
   }
   const isSubstring = (s1, s2) => {
     return s1.toLowerCase().includes(s2.toLowerCase())
-  } 
+  }
 
-const contacts = search.length? persons.filter(person => isSubstring(person.name,search)) : persons
- 
+  const contacts = search.length
+    ? persons.filter((person) => isSubstring(person.name, search))
+    : persons
+
   return (
     <div>
       <h2>Phonebook</h2>
 
-      <form>
-        <div>
-          filter shown with<input value={search} onChange={handleSearch} />
-        </div>
-      </form>
+      {/* Filter field */}
+      <Filter fieldName={search} onChange={handleSearch} />
 
       <h3>Add a new</h3>
-      <form onSubmit={addContact}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+
+      {/* Contacts field */}
+      <Form
+        onSubmit={addContact}
+        name={newName}
+        number={newNumber}
+        onNameChange={handleNameChange}
+        onNumberChange={handleNumberChange}
+      />
+
       <h3>Numbers</h3>
+
       <div>
         {contacts.map((person) => (
-          <Contacts key={person.name} name={person.name} number={person.number} />
+          <Contacts
+            key={person.name}
+            name={person.name}
+            number={person.number}
+          />
         ))}
       </div>
     </div>
---
+++ part2/phonebook/src/components/Filter.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Filter = ({ fieldName, onChange }) => {
+  return (
+    <>
+      <form>
+        <div>
+          filter shown with
+          <input value={fieldName} onChange={onChange} />
+        </div>
+      </form>
+    </>
+  )
+}
+export default Filter
