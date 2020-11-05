bc95464ffb9aa4e1a50cd304cf7f743d38a91031
Nadine-Fadlallah <nadine.fadlallah98@gmail.com>
2020-11-05 13:34:03 +0200

    Exercise 2.11 Solution

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,25 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
+import axios from 'axios'
 import Contacts from "./components/Contacts"
 import Form from "./components/Form"
 import Filter from "./components/Filter"
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" }
-  ])
+  const [persons, setPersons] = useState([])
 
   const [newName, setNewName] = useState("")
   const [newNumber, setNewNumber] = useState("")
   const [search, setNewSearch] = useState("")
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      console.log("response fullfied")
+      setPersons(response.data)
+    })
+  }, [])
 
   const addContact = (event) => {
     event.preventDefault()
@@ -27,9 +33,9 @@ const App = () => {
       .filter((person) => person.number === newNumber)
       .map((hit) => hit.number)
       .join()
-    // Check no repetion or empty field
+    // Check no repetion or empty field(s)
     if (!(repeatedName || repeatedNumber)) {
-      // show a message if one of the inputs is empty or both and don't reset
+      // show a message in case if empty input(s) & don't reset
       if (!(newName && newNumber)) {
         window.alert("Please fill the missing contact info!")
         return
