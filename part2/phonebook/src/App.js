1456fbcf512d7d8e272687304059c679df9c9512
Nadine-Fadlallah <nadine.fadlallah98@gmail.com>
2020-11-11 07:03:12 +0200

    Exercise 2.15 Solution

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,84 +1,86 @@
-import React, { useState, useEffect } from "react"
-import axios from 'axios'
-import Contacts from "./components/Contacts"
-import Form from "./components/Form"
-import Filter from "./components/Filter"
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Contacts from "./components/Contacts";
+import Form from "./components/Form";
+import Filter from "./components/Filter";
+//import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([])
+  const [persons, setPersons] = useState([]);
 
-  const [newName, setNewName] = useState("")
-  const [newNumber, setNewNumber] = useState("")
-  const [search, setNewSearch] = useState("")
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [search, setNewSearch] = useState("");
 
   useEffect(() => {
-    console.log('effect')
-    axios
-    .get('http://localhost:3001/persons')
-    .then(response => {
-      console.log("response fullfied")
-      setPersons(response.data)
-    })
-  }, [])
+    //console.log('effect')
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("response fullfied");
+      setPersons(response.data);
+    });
+  }, []);
 
   const addContact = (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
-    const repeatedName = persons
-      .filter((person) => person.name === newName)
-      .map((hit) => hit.name)
-      .join()
+    const repeatedName = persons.find((person) => person.name === newName); //returns the person that matches the entered name
 
-    const repeatedNumber = persons
-      .filter((person) => person.number === newNumber)
-      .map((hit) => hit.number)
-      .join()
+    const repeatedNumber = persons.find(
+      (person) => person.number === newNumber
+    ); //returns the person that matches the entered number
+
+    // console.log(repeatedName + '  ' + repeatedNumber)
+    
     // Check no repetion or empty field(s)
     if (!(repeatedName || repeatedNumber)) {
-      // show a message in case if empty input(s) & don't reset
+      // show a message in case of empty input(s) & don't reset
       if (!(newName && newNumber)) {
-        window.alert("Please fill the missing contact info!")
-        return
+        window.alert("Please fill the missing contact info!");
+        return;
       }
 
       const personObj = {
         name: newName,
-        number: newNumber
-      }
-      setPersons(persons.concat(personObj))
+        number: newNumber,
+      };
+      axios
+        .post("http://localhost:3001/persons", personObj)
+        .then((response) => {
+          //console.log(response)
+          setPersons(persons.concat(response.data));
+        });
+      //setPersons(persons.concat(personObj))
     }
     // In case of a repeated name or number
     else {
       window.alert(
-        `${
-          repeatedName.length ? newName : newNumber
-        } is already added to phonebook`
-      )
+        `${repeatedName ? newName : newNumber} is already added to phonebook`
+      );
     }
-    //console.log("Repeated: " + repeatedName + repeatedNumber)
-    setNewName("")
-    setNewNumber("")
-  }
+
+    setNewName("");
+    setNewNumber("");
+  };
   const handleNameChange = (event) => {
-    console.log(event.target.value)
-    setNewName(event.target.value)
-  }
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
 
   const handleNumberChange = (event) => {
-    console.log(event.target.value)
-    setNewNumber(event.target.value)
-  }
+    console.log(event.target.value);
+    setNewNumber(event.target.value);
+  };
 
   const handleSearch = (event) => {
-    setNewSearch(event.target.value)
-  }
+    setNewSearch(event.target.value);
+  };
   const isSubstring = (s1, s2) => {
-    return s1.toLowerCase().includes(s2.toLowerCase())
-  }
+    return s1.toLowerCase().includes(s2.toLowerCase());
+  };
 
   const contacts = search.length
     ? persons.filter((person) => isSubstring(person.name, search))
-    : persons
+    : persons;
 
   return (
     <div>
@@ -110,7 +112,7 @@ const App = () => {
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
